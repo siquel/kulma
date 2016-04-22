691a7226cfe57368a371a2ff29f9a79bbc198a55
siquel <squual@kahvipaussi.net>
2016-04-22 17:30:09 +0300

    Add platform macros

--- examples/00-helloworld/helloworld.cpp
+++ examples/00-helloworld/helloworld.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "../src/platform.h"
 
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
-    printf("Hello world\n");
+    printf("Hello world! Running on %s, target arch %s, compiled with %s\n", 
+        KULMA_PLATFORM_NAME,
+        KULMA_ARCH_NAME,
+        KULMA_COMPILER_NAME
+    );
     return 0; 
 }
