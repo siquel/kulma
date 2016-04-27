eb12280ec38fda6570b317dd07d6cfed66c58f2f
siquel <squual@kahvipaussi.net>
2016-04-28 00:00:49 +0300

    Add toolset as commandline parameter

--- vshax.bat
+++ vshax.bat
@@ -1,2 +1,2 @@
-powershell -Command "(gc .build/projects/vs2015/unit_test.vcxproj) -replace '<ExceptionHandling>false', '<ExceptionHandling>Sync' | Out-File -encoding UTF8 .build/projects/vs2015/unit_test.vcxproj"
-powershell -Command "(gc .build/projects/vs2015/unit_test.vcxproj) -replace '<RuntimeTypeInfo>false', '<RuntimeTypeInfo>true' | Out-File -encoding UTF8 .build/projects/vs2015/unit_test.vcxproj"
+powershell -Command "(gc .build/projects/%1/unit_test.vcxproj) -replace '<ExceptionHandling>false', '<ExceptionHandling>Sync' | Out-File -encoding UTF8 .build/projects/%1/unit_test.vcxproj"
+powershell -Command "(gc .build/projects/%1/unit_test.vcxproj) -replace '<RuntimeTypeInfo>false', '<RuntimeTypeInfo>true' | Out-File -encoding UTF8 .build/projects/%1/unit_test.vcxproj"
