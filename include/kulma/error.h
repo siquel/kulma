903bf09dec235cccdb969038271df11d57761bfb
siquel <squual@kahvipaussi.net>
2016-06-02 16:57:21 +0300

    Create error interface

---
+++ include/kulma/error.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <stdint.h>
+#include "debug.h"
+
+#define KULMA_ERROR_SET(ptr, result, msg)                                   \
+            do {                                                            \
+                KULMA_TRACE("Error %d: %s", result.m_code, "" msg);         \
+                ptr->setError(result, "" msg);                              \
+            while (0)
+
+#define KULMA_ERROR_RESULT(err, code)                                       \
+            static_assert(code != 0, "Error code 0 is reserved");           \
+            static const kulma::ErrorResult err = { code }
+
+namespace kulma
+{
+    // just a wrapper
+    struct ErrorResult
+    {
+        uint32_t m_code;
+    };
+
+    class Error
+    {
+    public:
+        Error(const Error&) = delete;
+        Error& operator=(const Error&) = delete;
+        Error(const Error&&) = delete;
+        Error& operator=(const Error&&) = delete;
+
+        Error()
+            : m_code(0)
+        {
+
+        }
+
+        void setError(ErrorResult p_error, const char* p_msg)
+        {
+            // 0 means success
+            KULMA_ASSERT(p_error.m_code != 0, "Invalid error code passed to setError, 0 is reserved error code!");
+
+            if (!isOk()) return;
+
+            m_code = p_error.m_code;
+            m_msg = p_msg;
+        }
+
+        ErrorResult get() const
+        {
+            return ErrorResult{ m_code };
+        }
+
+        bool isOk() const
+        {
+            return m_code == 0;
+        }
+
+        bool operator==(ErrorResult p_rhs) const 
+        {
+            return m_code == p_rhs.m_code;
+        }
+    private:
+        uint32_t m_code; ///< FOURCC code
+        const char* m_msg; ///< Error message
+    };
+
+    class ScopedError
+    {
+    public:
+        ScopedError(Error* p_err)
+            : m_error(p_err)
+        {
+            KULMA_ASSERT(p_err != nullptr, "p_err can not be null");
+        }
+
+        ~ScopedError()
+        {
+            KULMA_ASSERT(m_error->isOk(), "Error: %d", m_error->get().m_code);
+        }
+    private:
+        Error* m_error;
+    };
+}
