756272694cc42964bffa56df8289e67af52a21fc
siquel <squual@kahvipaussi.net>
2016-06-07 16:04:19 +0300

    Implement file open

--- include/kulma/io/file.h
+++ include/kulma/io/file.h
@@ -8,21 +8,39 @@ namespace kulma
     {
         enum Enum
         {
-            Open,
-            Write,
-            Truncate
+            Read,
+            Write
         };
     };
 
     struct KULMA_NO_VTABLE IFile 
     {
+        /// \brief Destructor
         virtual ~IFile() = 0;
 
+        /// \brief Opens the file for reading/writing
+        /// 
+        /// \param p_path The file path
+        /// \param p_mode In which mode the file should be opened
+        ///
+        /// \return True if opened successfully
+        virtual bool open(const char* p_path, FileOpenMode::Enum p_mode) = 0;
+
+        /// \brief Closes the file
         virtual void close() = 0;
 
-        /// \brief 
+        /// \brief Checks if the file is open
+        ///
+        /// \return True if the file is open
+        virtual bool is_open() const = 0;
+
+        /// \brief
         virtual int32_t read(void* p_data, int32_t p_size) = 0;
     };
+
+    KULMA_ERROR_RESULT(KULMA_ERROR_IO_OPEN, KULMA_MAKEFOURCC('I', 'O', 0, 1));
+    KULMA_ERROR_RESULT(KULMA_ERROR_IO_READ, KULMA_MAKEFOURCC('I', 'O', 0, 2));
+    KULMA_ERROR_RESULT(KULMA_ERROR_IO_WRITE, KULMA_MAKEFOURCC('I', 'O', 0, 3));
 
     class FileReader : public IFileReader
     {
@@ -42,6 +60,8 @@ namespace kulma
 
         /// @copydoc IClose::close()
         virtual void close() override;
+    private:
+        IFile* m_file;
     };
 }
 
--- src/io/file.cpp
+++ src/io/file.cpp
@@ -1,8 +1,67 @@
 #include "kulma/io/file.h"
 
+#include "kulma/platform.h"
+#if KULMA_PLATFORM_WINDOWS
+#   ifndef WIN32_LEAN_AND_MEAN
+#       define WIN32_LEAN_AND_MEAN
+#   endif
+#   include <Windows.h>
+#elif KULMA_PLATFORM_LINUX
+
+#endif
+
 namespace kulma
 {
+    class DiskFile : public IFile
+    {
+    public:
+        DiskFile()
+#if KULMA_PLATFORM_WINDOWS
+            : m_file(INVALID_HANDLE_VALUE)
+#elif KULMA_PLATFORM_LINUX
+            : m_file(NULL)
+#endif
+        {
+
+        }
+
+        bool open(const char* p_path, FileOpenMode::Enum p_mode) override
+        {
+#if KULMA_PLATFORM_WINDOWS
+            m_file = CreateFile(
+                p_path,
+                (p_mode == FileOpenMode::Read) ? GENERIC_READ : GENERIC_WRITE,
+                0,
+                NULL,
+                OPEN_ALWAYS,
+                FILE_ATTRIBUTE_NORMAL,
+                NULL
+                );
+            KULMA_ASSERT(m_file != INVALID_HANDLE_VALUE,
+                "CreateFile failed, GetLastError = %d, path = %s",
+                GetLastError(),
+                p_path);
+            return m_file != INVALID_HANDLE_VALUE;
+#elif KULMA_PLATFORM_LINUX
+            m_file = fopen(p_path, (p_mode == FileOpenMode::Read) ? "rb" : "wb");
+            KULMA_ASSERT(m_file != NULL,
+                "fopen: errno = %d, path = %s",
+                errno,
+                p_path);
+            return m_file != NULL;
+#endif
+        }
+
+    private:
+#if KULMA_PLATFORM_WINDOWS
+        HANDLE m_file;
+#elif KULMA_PLATFORM_LINUX
+        FILE* m_file;
+#endif
+    };
+
     FileReader::FileReader()
+        : m_file(NULL)
     {
 
     }
@@ -26,8 +85,16 @@ namespace kulma
 
     bool FileReader::open(const char * p_filePath, Error * p_err)
     {
-        KULMA_UNUSED(p_filePath, p_err);
-        return false;
+        KULMA_ASSERT(p_err != NULL, "Reading interface error handler can't be null");
+        KULMA_ASSERT(m_file != NULL, "File can't be null");
+
+        if (m_file->open(p_filePath, FileOpenMode::Read))
+        {
+            KULMA_ERROR_SET(p_err, KULMA_ERROR_IO_OPEN, "FileReader: Failed to open file");
+            return false;
+        }
+        
+        return true;
     }
 
     void FileReader::close()
